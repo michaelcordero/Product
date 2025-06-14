63becc3a56c6072b62e8a212985fd18b0b64193e
michaelcordero <michaelpetercordero@gmail.com>
2025-06-14 15:14:30 -0500

    * added TestContainers so the appliction context could load (DBs included) during JUnit tests.

--- src/test/java/org/grainger/product/ProductApplicationTests.java
+++ src/test/java/org/grainger/product/ProductApplicationTests.java
@@ -2,9 +2,15 @@ package org.grainger.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-class ProductApplicationTests {
+@SpringBootTest(classes = ProductApplication.class)
+@Testcontainers
+@TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=create"
+})
+class ProductApplicationTests extends TestContainerDBSupport {
 
     @Test
     void contextLoads() {
---
+++ src/test/java/org/grainger/product/TestContainerDBSupport.java
@@ -0,0 +1,28 @@
+package org.grainger.product;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
+public abstract class TestContainerDBSupport {
+
+    @SuppressWarnings("resource")
+    @Container
+    static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:14")
+            .withDatabaseName("pgaspringreact")
+            .withUsername("product-user")
+            .withPassword("product-user-password");
+
+    @DynamicPropertySource
+    static void configure(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgreSQLContainer::getDriverClassName);
+    }
+}
